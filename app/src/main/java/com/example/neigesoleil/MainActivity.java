74e62b4d0bf6f32d44f3f512b9c0ab8aa362c881
Shan <shan.azizh@gmail.com>
2021-04-11 17:53:43 +0200

    activity_main, Mise en forme et création des élements

--- app/src/main/java/com/example/neigesoleil/MainActivity.java
+++ app/src/main/java/com/example/neigesoleil/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.neigesoleil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnConnexion;
+    private TextView txtUsername;
+    private TextView txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnConnexion = (Button) findViewById(R.id.idConnexion);
+        this.txtUsername = (TextView) findViewById(R.id.idUsername);
+        this.txtPassword = (TextView) findViewById(R.id.idPassword);
     }
 }
